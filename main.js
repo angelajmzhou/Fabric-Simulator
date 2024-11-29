4d0f3eea43cf4414b700a5ea3fc7cc2621624371
angelajzhou <155860768+angelajmzhou@users.noreply.github.com>
2024-11-28 16:29:38 -0800

    physics working?

--- main.js
+++ main.js
@@ -4,49 +4,50 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import Physics from './physics.js';
 
 // Scene Setup
-const physicsWorld = new Physics();
-await physicsWorld.init();
-console.log("Physics world ready.")
-// Get the canvas element
-const canvas = document.getElementById('canvas');
+Ammo().then(function(Ammo) {
+	const physicsWorld = new Physics();
+	console.log("Physics world ready.")
+	// Get the canvas element
+	const canvas = document.getElementById('canvas');
+	
+	// Scene Setup
+	const scene = new THREE.Scene();
+	const camera = new THREE.PerspectiveCamera(85, window.innerWidth / window.innerHeight, 0.1, 1000);
+	camera.position.set(0, 2, 5);
+	
+	const renderer = new THREE.WebGLRenderer({ canvas });
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setClearColor(0xA3A3A3);
+	
+	// Lighting
+	const light = new THREE.DirectionalLight(0xffffff, 1);
+	light.position.set(5, 10, 7);
+	scene.add(light);
+	
+	// Load Mannequin
+	const loader = new FBXLoader();
+	let mannequin;
+	loader.load('Female_Body_Base_Model.fbx', (fbx) => {
+	  mannequin = fbx;
+	  mannequin.scale.set(0.001, 0.001, 0.001);
+	  scene.add(mannequin);
+	});
+	
+	// Camera Controls
+	const controls = new OrbitControls(camera, renderer.domElement);
+	
+	// Animation Loop
+	let prevTime = performance.now();
+	function animate() {
+	  requestAnimationFrame(animate);
+	  //const currentTime = performance.now();
+	  //const deltaTime = (currentTime - prevTime) / 1000;
+	  //prevTime = currentTime;
+	
+	  //if (mannequin) simulate(deltaTime);
+	  renderer.render(scene, camera);
+	}
+	animate();
 
-// Scene Setup
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(85, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 2, 5);
-
-const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0xA3A3A3);
-
-// Lighting
-const light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(5, 10, 7);
-scene.add(light);
-
-// Load Mannequin
-const loader = new FBXLoader();
-let mannequin;
-loader.load('Female_Body_Base_Model.fbx', (fbx) => {
-  mannequin = fbx;
-  mannequin.scale.set(0.001, 0.001, 0.001);
-  scene.add(mannequin);
 });
-
-// Camera Controls
-const controls = new OrbitControls(camera, renderer.domElement);
-
-// Animation Loop
-let prevTime = performance.now();
-function animate() {
-  requestAnimationFrame(animate);
-  //const currentTime = performance.now();
-  //const deltaTime = (currentTime - prevTime) / 1000;
-  //prevTime = currentTime;
-
-  //if (mannequin) simulate(deltaTime);
-  renderer.render(scene, camera);
-}
-animate();
-
 
